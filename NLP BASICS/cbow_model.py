91feb4b5b587b4377c84f2ea90c521b0f82e841b
Hafiz Shakeel Ahmad Awan <44207455+hafizshakeel@users.noreply.github.com>
2024-08-20 13:15:01 +0900

    Update cbow_model.py

--- NLP BASICS/cbow_model.py
+++ NLP BASICS/cbow_model.py
@@ -1,3 +1,15 @@
+"""
+Natural Language Processing Model Implementation
+
+Efficient Estimation of Word Representations in Vector Space
+(CBOW) Implementation: https://arxiv.org/abs/1301.3781
+
+This script contains the implementation of the CBOW model.
+
+Implementation by: Hafiz Shakeel Ahmad Awan
+Email: hafizshakeel1997@gmail.com
+"""
+
 import os
 
 import torch
